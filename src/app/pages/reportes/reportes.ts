7f6c1e3be5f79f456a6aa01a8d4cb25e1d946998
Jonathan Piñones <jonathan.pinones@supereduc.cl>
2026-05-11 16:31:24 -0400

    se agrega card a indicadores de reportes

--- src/app/pages/reportes/reportes.ts
+++ src/app/pages/reportes/reportes.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Expediente } from '../../../models/expediente';
 
 @Component({
   selector: 'app-reportes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reportes.html',
   styleUrl: './reportes.css',
 })
-export class Reportes {}
+export class Reportes implements OnInit {
+  expedientes: Expediente[] = [];
+
+  ngOnInit() {
+    const data = localStorage.getItem('expedientes');
+    if (data) {
+      this.expedientes = JSON.parse(data);
+    }
+  }
+
+  get totalRegistrado(): number {
+    return this.expedientes.length;
+  }
+
+  get pendienteDeGestion(): number {
+    return this.expedientes.filter(e => e.estado === 'Pendiente').length;
+  }
+
+  get procesoTerminado(): number {
+    return this.expedientes.filter(e => e.estado === 'Finalizado').length;
+  }
+}
